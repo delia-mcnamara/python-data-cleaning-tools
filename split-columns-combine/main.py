69051a9a522afd69d04f670f321e17a386f09f87
David Vogel <david@davidvogel.xyz>
2025-08-23 18:44:36 -0600

    UPDATE: completed `split-columns-combine`

---
+++ split-columns-combine/main.py
@@ -0,0 +1,82 @@
+import fnmatch
+import glob
+import os
+import pandas as pd
+import sys
+
+
+EXPORT_DIR = "clean"
+
+
+def split_columns_combine(df):
+    df_left = df[["number", "letter", "color"]]
+    df_right = df[["number.1", "letter.1", "color.1"]]
+
+    df_right.columns = df_left.columns
+
+    df = pd.concat([df_left, df_right], ignore_index=True)
+
+    return df
+
+
+def process_csv(file):
+    print("PROCESSING:", file)
+
+    try:
+        df = pd.read_csv(file)
+        df = split_columns_combine(df)
+
+        df.to_csv(
+            os.path.join(EXPORT_DIR, os.path.basename(file)),
+            encoding='utf-8',
+            index=False,
+            header=True
+        )
+    except Exception as err:
+        print("ERROR:", file, "not split --", err)
+
+
+def ingest_path(path):
+    if os.path.isfile(path):
+        if not fnmatch.fnmatch(path, "*.csv"):
+            print(path, "is not a CSV file, you silly goose!")
+        else:
+            process_csv(path)
+        return
+
+    if os.path.isdir(path):
+        for file in glob.glob(os.path.join(path, "*.csv")):
+            process_csv(file)
+        return
+
+    print(path, "is not an existing path.")
+
+
+def parse_args():
+    # Exits when no argument is passed
+    if len(sys.argv) < 2:
+        sys.exit("ERROR: No file was passed. Please pass a file to continue.")
+
+    # in case of any file processing, confirm the state of the export directory
+    if not os.path.isdir(EXPORT_DIR):
+        os.makedirs(EXPORT_DIR)
+
+    # Handles when 1 argument is passed
+    if len(sys.argv) == 2:
+        path = sys.argv[1]
+        ingest_path(path)
+
+    # Handles when more than 1 argument is passed
+    if len(sys.argv) > 2:
+        for path in sys.argv[1:]:
+            ingest_path(path)
+
+
+def main():
+    parse_args()
+
+    print("Processed all files!")
+
+
+if __name__ == "__main__":
+    main()
